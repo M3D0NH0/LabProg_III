5c4f4cd56b983494839c68d50e1abec8f8741d07
msndogui@hotmail.com.br <msndogui@hotmail.com.br>
2014-05-06 11:32:22 -0300

    Adicionando um construtor com tamanho fixo

--- Projeto_Desenho/src/projetodesenho/Quadrado.java
+++ Projeto_Desenho/src/projetodesenho/Quadrado.java
@@ -14,6 +14,14 @@ import java.awt.Graphics;
  */
 public class Quadrado extends Figura{
     private int x,y,tam;
+    
+    
+    
+    public Quadrado(int x, int y){
+        this.x = x;
+        this.y = y;
+        this.tam = 10;
+    }
     public Quadrado(int x, int y, int tam){
         this.x = x;
         this.y = y;
